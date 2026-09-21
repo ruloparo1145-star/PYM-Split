9ee6af02c72e23d7a7c2d99a71bc47a5d745ca55
ruloparo1145-star <ruloparo1145@gmail.com>
2026-09-21 22:16:00 +0200

    Delete js/config.js

--- js/config.js
+++
@@ -1,8 +0,0 @@
-// js/config.js
-export const SUPABASE_URL = 'https://gwqyzubncmvfxnnqyqfs.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_9IFxrFf4qrqEwT0nArNLzg_PixbxgYt';
-
-// Configuración general de la app
-export const APP_NAME = 'PYM Split';
-export const APP_VERSION = '1.0.0';
-export const DEFAULT_CURRENCY = 'EUR';
