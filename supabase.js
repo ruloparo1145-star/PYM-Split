70247e5417e21ee38a6b27bec4766d0dc579a054
ruloparo1145-star <ruloparo1145@gmail.com>
2026-09-21 22:21:30 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,5 +0,0 @@
-// js/supabase.js
-import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
