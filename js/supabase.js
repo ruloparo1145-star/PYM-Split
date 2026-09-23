1cbd878fecbbb5c245091cd6c4b2bb648b4b7536
ruloparo1145-star <ruloparo1145@gmail.com>
2026-09-23 06:25:30 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,7 +5,6 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Los encontras en: Supabase -> Project Settings -> API Keys
 const SUPABASE_URL = 'https://gwqyzubncmvfxxnnyqfs.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_9IFxrFf4qrqEwTOnArNLzg_PixbxgYt';
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 console.log('Supabase conectado correctamente a:', SUPABASE_URL);
