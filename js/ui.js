e84d3bdfdf542a0b2803e801f3b8d7433fda8ed7
ruloparo1145-star <ruloparo1145@gmail.com>
2026-09-23 20:31:17 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -26,6 +26,7 @@ import { initAddMemberModal } from './members.js';
   initGroupModal();
   initFriendsModal();
   initExpenseModal();
+  initFiltroCategoria();
   initAddMemberModal();
   initArchivedToggle();
 })();
